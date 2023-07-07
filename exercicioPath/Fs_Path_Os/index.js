c6ef11f4fc424a3aa8ebfe60fd9ffc05a2739405
Willyanne Silva <110118532+maiiyx@users.noreply.github.com>
2023-07-07 17:38:01 -0300

    Add files via upload

---
+++ exercicioPath/Fs_Path_Os/index.js
@@ -0,0 +1,78 @@
+// FS
+
+// atv1
+// **********************************************************
+// const fs = require ('fs')
+
+// fs.appendFile('arquivo1.txt', 'segredo', function(err){
+//     console.log('Os cavalo-marinho macho que engravidam. 🪸')
+// })
+
+// **********************************************************
+
+// atv2
+// **********************************************************
+// const fs = require ('fs')
+
+// fs.writeFile('arquivo2.txt', 'tchooow', function(err){
+//     console.log('pegaaa')
+// })
+
+// **********************************************************
+
+// atv3
+// **********************************************************
+// const fs = require ('fs')
+
+// fs.appendFile('arquivo2.txt', 'viu que funcionou 😌', function(err){
+//     console.log('vai dar certo')
+// })
+
+// **********************************************************
+
+// atv4
+// **********************************************************
+// const fs = require('fs')
+
+// fs.copyFile('arquivo3.txt', 'arquivo1.txt', function(err){
+//     console.log('segredo')
+// })
+
+// **********************************************************
+
+// atv5
+// **********************************************************
+// const fs = require('fs')
+
+// const arqAntigo = 'antigo.txt'
+// const arqNovo = 'novo.txt'
+
+// fs.rename(arqAntigo, arqNovo, (err)=>{
+//     if(err){
+//         console.log(err)
+//         return
+//     }
+//     console.log(`O arquivo ${arqAntigo} foi renomeado para ${arqNovo}`)
+// })
+
+// **********************************************************
+
+// atv6
+// **********************************************************
+// const fs = require('fs')
+
+// fs.unlink('delet.txt', (err)=>{
+//     if(err){
+//         console.log(err)
+//         return
+//     }
+//     console.log("Arquivo deletado! 👺")
+// })
+
+// **********************************************************
+
+// atv7
+// **********************************************************
+
+
+// **********************************************************
